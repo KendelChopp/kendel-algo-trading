1b888dbbbc673f633bc1cb72d63948be0e732ba3
Kendel Chopp <kendelchopp@gmail.com>
2020-09-06 21:18:13 -0500

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const _ = require('lodash');
-const Alpaca = require('@alpacahq/alpaca-trade-api');
-const SMA = require('technicalindicators').SMA;
+import _ from 'lodash';
+import Alpaca from '@alpacahq/alpaca-trade-api';
+import Backtest from '@kendelchopp/alpaca-js-backtesting';
+import TechnicalIndicators from 'technicalindicators';
+const { SMA } = TechnicalIndicators;
 
 const alpaca = new Alpaca({
   keyId: process.env.API_KEY,
   secretKey: process.env.SECRET_API_KEY,
-  paper: true
+  paper: true,
+  usePolygon: false
+});
+
+const backtest = new Backtest({
+  alpaca,
+  startDate: new Date(2020, 6, 1),
+  endDate: new Date(2020, 10, 1)
 });
 
 let sma20, sma50;
@@ -21,7 +30,7 @@ async function initializeAverages() {
     }
   );
 
-  const closeValues = _.map(initialData.SPY, (bar) => bar.c);
+  const closeValues = _.map(initialData.SPY, (bar) => bar.closePrice);
 
   sma20 = new SMA({ period: 20, values: closeValues });
   sma50 = new SMA({ period: 50, values: closeValues });
@@ -30,47 +39,53 @@ async function initializeAverages() {
   console.log(`sma50: ${sma50.getResult()}`);
 }
 
-initializeAverages();
+//initializeAverages();
 
-const client = alpaca.websocket;
+sma20 = new SMA({ period: 20, values: [] });
+sma50 = new SMA({ period: 50, values: [] });
+
+//const client = alpaca.data_ws;
+const client = backtest.data_ws;
 
 client.onConnect(() => {
   client.subscribe(['AM.SPY']);
-  setTimeout(() => client.disconnect(), 6000*1000);
+  //setTimeout(() => client.disconnect(), 6000*1000);
 });
 
 client.onStockAggMin((subject, data) => {
-  const nextValue = JSON.parse(data)[0].c;
+  const nextValue = JSON.parse(data)[0].closePrice;
 
   const next20 = sma20.nextValue(nextValue);
   const next50 = sma50.nextValue(nextValue);
-
-  console.log(`next20: ${next20}`);
-  console.log(`next50: ${next50}`);
+  //
+  // console.log(`next20: ${next20}`);
+  // console.log(`next50: ${next50}`);
 
   if (next20 > next50 && lastOrder !== 'BUY') {
-    alpaca.createOrder({
+    backtest.createOrder({
       symbol: 'SPY',
-      qty: 300,
+      qty: 30,
       side: 'buy',
       type: 'market',
       time_in_force: 'day'
     });
 
     lastOrder = 'BUY';
-    console.log('\nBUY\n');
   } else if (next20 < next50 && lastOrder !== 'SELL') {
-    alpaca.createOrder({
+    backtest.createOrder({
       symbol: 'SPY',
-      qty: 300,
+      qty: 30,
       side: 'sell',
       type: 'market',
       time_in_force: 'day'
     });
 
     lastOrder = 'SELL';
-    console.log('\nSELL\n');
   }
+});
+
+client.onDisconnect(() => {
+  console.log(backtest.getStats());
 });
 
 client.connect();
